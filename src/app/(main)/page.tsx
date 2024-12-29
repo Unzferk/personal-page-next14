6bba4f50d73cd79ecef329d3f180b96a9958ebe9
Fernando Unzueta <fernando.unzueta.dev@gmail.com>
2024-12-29 17:55:02 -0400

    T1 base layout

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@/components/Container'
+
+export default async function Home() {
+  return (
+    <div className="pb-12 pt-16 sm:pb-4 lg:pt-12">
+      <Container>
+        <h1 className="text-2xl font-bold leading-7 text-slate-900">
+          Episodes
+        </h1>
+      </Container>
+      <div className="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100"></div>
+    </div>
+  )
+}
